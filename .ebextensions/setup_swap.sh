92fe68843f23a80a1f3a902ab6c6392f4a6edd11
Clement <clement.bisaillon@hotmail.com>
2019-10-22 19:22:14 -0400

    swap

--- .ebextensions/setup_swap.sh
+++ .ebextensions/setup_swap.sh
@@ -1,11 +1,19 @@
-#!/bin/bash
-SWAPFILE=/var/swapfile
-SWAP_MEGABYTES=1024
-if [ -f $SWAPFILE ]; then
-	echo "Swapfile $SWAPFILE found, assuming already setup"
-	exit;
+#!/bin/sh
+
+# size of swapfile in megabytes
+swapsize=1024
+
+# does the swap file already exist?
+grep -q "swapfile" /etc/fstab
+
+# if not then create it
+if [ $? -ne 0 ]; then
+  echo 'swapfile not found. Adding swapfile.'
+  fallocate -l ${swapsize}M /swapfile
+  chmod 600 /swapfile
+  mkswap /swapfile
+  swapon /swapfile
+  echo '/swapfile none swap defaults 0 0' >> /etc/fstab
+else
+  echo 'swapfile found. No changes made.'
 fi
-/bin/dd if=/dev/zero of=$SWAPFILE bs=1M count=$SWAP_MEGABYTES
-/bin/chmod 600 $SWAPFILE
-/sbin/mkswap $SWAPFILE
-/sbin/swapon $SWAPFILE
